cda01ee807e367de90e5876dafcc1126b8f683ff
Maciej Blazejczak <mblazejczak@sii.pl>
2023-02-20 14:45:38 +0100

    - added test case to verify status code  - added test case to verify headers  - added test case to verify url

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -1,10 +1,16 @@
-from http import HTTPStatus
-from endpoints_functions.user_api_helpers import post_create_user
+from endpoints_functions.user_api_helpers import post_create_user, new_user_body
 
 
+def test_create_user_status_code():
+    response = post_create_user(user=new_user_body)
+    assert response.status_code == 200
 
-def test_create_user():
-    assert post_create_user.status_code == HTTPStatus.OK
-    print(post_create_user)
+
+def test_create_user_headers():
+    response = post_create_user(user=new_user_body)
+    assert response.headers['Access-Control-Allow-Methods'] == 'GET, POST, DELETE, PUT'
 
 
+def test_create_user_url():
+    response = post_create_user(user=new_user_body)
+    assert response.url == 'https://petstore.swagger.io/v2/user'
