6a9f9c050352241baf704f7a85ac68a24489106a
Maciej Blazejczak <mblazejczak@sii.pl>
2023-02-28 12:26:37 +0100

    - added pet_endpoints.py,    - implementation of all endpoints from 'pet' section,  - added pet_tests.py,    - added 11 test cases to verify corectness of endpoints within 'pet' section,  - added useful_stuff.py

--- endpoints_functions/useful_stuff.py
+++ endpoints_functions/useful_stuff.py
@@ -1,6 +1,4 @@
-# simple id generator
 import random
-
 
 
 def generate_id():
@@ -8,19 +6,29 @@ def generate_id():
     return generated_id
 
 
+def generate_category_name():
+    category_name_list = ['dog', 'cat', 'fish', 'turtle', 'snake']
+    category_name = random.choice(category_name_list)
+    return category_name
+
+
+def generate_tags_name():
+    tags_name_list = ['wild', 'pet']
+    tags_name = random.choice(tags_name_list)
+    return tags_name
+
+
 # class to create a pet with category_name, name and tags_name and random_id
 class Pet:
-    def __init__(self, category_name, name, tags_name):
-        self.category_name = category_name
+    def __init__(self, name):
         self.name = name
-        self.tags_name = tags_name
 
     def create_pet(self):
         basic_pet = {
             "id": generate_id(),
             "category": {
                 "id": generate_id(),
-                "name": self.category_name
+                "name": generate_category_name()
             },
             "name": self.name,
             "photoUrls": [
@@ -29,7 +37,7 @@ class Pet:
             "tags": [
                 {
                     "id": generate_id(),
-                    "name": self.tags_name
+                    "name": generate_tags_name()
                 }
             ],
             "status": "available"
---
+++ tests/pet_tests.py
@@ -0,0 +1,117 @@
+from http import HTTPStatus
+from endpoints_functions import pet_endpoints
+
+
+def test_add_a_new_pet_status_code():
+    """
+    Test: Add a new pet to the store, check status code
+    method: POST
+    endpoint: https://petstore.swagger.io/v2/pet
+    """
+    response = pet_endpoints.add_new_pet()[0]
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_add_a_new_pet_header():
+    """
+    Test: Add a new pet to the store, check header
+    method: POST
+    endpoint: https://petstore.swagger.io/v2/pet
+    """
+    response = pet_endpoints.add_new_pet()[0]
+    assert response.headers['Access-Control-Allow-Methods'] == 'GET, POST, DELETE, PUT'
+
+
+def test_finding_pet():
+    """
+    Test: Find pet by ID
+    method: GET
+    endpoint: https://petstore.swagger.io/v2/pet/{id}
+    """
+    response = pet_endpoints.find_pet_by_id()
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_finding_pet_by_status_status_code():
+    """
+    Test: Find pet by status, check status code
+    method: GET
+    endpoint: https://petstore.swagger.io/v2/pet/findByStatus
+    """
+    response = pet_endpoints.find_pet_by_status()
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_finding_pet_by_status_url():
+    """
+    Test: Find pet by status, check url contains 'findByStatus'
+    method: GET
+    endpoint: https://petstore.swagger.io/v2/pet/findByStatus
+    """
+    response = pet_endpoints.find_pet_by_status()
+    assert '/findByStatus?status=' in response.url
+
+
+def test_update_a_pet_with_form_data_status_code():
+    """
+    Test: Update a pet in the store with form data, check status code
+    method: POST
+    endpoint: https://petstore.swagger.io/v2/pet/{id}
+    """
+    response = pet_endpoints.update_a_pet_with_form_data()
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_update_a_pet_with_form_data_check_message():
+    """
+    Test: Update a pet in the store with form data, check if message is equal to pet id
+    method: POST
+    endpoint: https://petstore.swagger.io/v2/pet/{id}
+    """
+    original_pet = pet_endpoints.add_new_pet()[1]
+    updated_pet = pet_endpoints.update_a_pet_with_form_data()
+    assert updated_pet.json()["message"] == original_pet
+
+
+def test_update_an_existing_pet_status_code():
+    """
+    Test: Update an existing pet in the store, check status code
+    method: PUT
+    endpoint: https://petstore.swagger.io/v2/pet/
+    """
+    response = pet_endpoints.update_an_existing_pet()
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_update_an_existing_pet_compare_names():
+    """
+    Test: Update an existing pet in the store, check if name is changed
+    method: PUT
+    endpoint: https://petstore.swagger.io/v2/pet/
+    """
+    original_pet = pet_endpoints.add_new_pet()[2]
+    updated_pet = pet_endpoints.update_an_existing_pet()
+    assert updated_pet.json()["name"] != original_pet
+
+
+
+def test_delete_pet():
+    """
+    Test: Delete a pet, check status code
+    method: DELETE
+    endpoint: https://petstore.swagger.io/v2/pet/{id}
+    """
+    deleted_pet = pet_endpoints.delete_a_pet()
+    assert deleted_pet.status_code == HTTPStatus.OK
+
+
+def test_delete_pet_compare_id():
+    """
+    Test: Delete a pet, check if message is equal to pet id
+    method: DELETE
+    endpoint: https://petstore.swagger.io/v2/pet/{id}
+    :return:
+    """
+    original_pet = pet_endpoints.add_new_pet()[1]
+    deleted_pet = pet_endpoints.delete_a_pet()
+    assert deleted_pet.json()["message"] == original_pet
