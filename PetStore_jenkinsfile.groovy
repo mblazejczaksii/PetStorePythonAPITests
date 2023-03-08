fa6dc111ec83e5df05b295493f89cd750478745c
Maciej Blazejczak <mblazejczak@sii.pl>
2023-03-08 12:36:52 +0100

    - added jenkinsfile to test build by push

---
+++ PetStore_jenkinsfile.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+node {
+    stage('Preparation') {
+        checkout scm
+    }
+    stage('Who am i?') {
+        echo "This job was triggered by a Git push to branch: ${env.BRANCH_NAME}"
+    }
+}
